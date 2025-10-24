2e8ab9dd0f29e98a1e929beca292f29400dd037e
mops010 <slon.boriskin@gmail.com>
2025-10-24 12:10:20 +0300

    update lab3

--- src/lab3/text_stats.py
+++ src/lab3/text_stats.py
@@ -1,7 +1,7 @@
 import sys
 from lib.text import normalize, tokenize, count_freq, top_n
 
-line = "Привет, мир! Привет!!!" # sys.stdin.readline()
+line = "привет абота абота привет" # sys.stdin.readline()
 
 text = tokenize(normalize(line))
 
@@ -12,14 +12,14 @@ def info(Beautiful=False):
         print(f'слово{' ' * (max_len - 5)} | частота')
         print(f'{'-' * (max_len + 10)}')
 
-        for _ in top_n(count_freq(text)):
+        for _ in sorted(top_n(count_freq(text))):
             print(f'{_[0]}{' ' * (max_len - len(_[0]))} | {_[1]}')
         return
     else:
         print(f'Всего слов: {len(text)}')
         print(f'Уникальных слов: {len(set(text))}')
         print(f'Топ-5:')
-        for _ in top_n(count_freq(text)):
+        for _ in sorted(top_n(count_freq(text))):
             print(f'{_[0]}:{_[1]}')
 
 info(Beautiful=True)
